8251282792c47dad775e731d0e35d83da519a700
Adri van Kempen <adrivankempen@gmail.com>
2025-05-12 23:19:46 +0200

    [fix] Resolved issues with premake

--- premake5.lua
+++ premake5.lua
@@ -1,8 +1,7 @@
 project "GLFW"
   kind "StaticLib"
   language "C"
-  staticruntime "off"
-  warnings "off"
+  staticruntime "on"
 
   targetdir ("bin/" .. outputdir .. "/%{prj.name}")
   objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -15,10 +14,12 @@ project "GLFW"
     "src/init.c",
 		"src/input.c",
     "src/monitor.c",
-    "src/null_init.c",
+
+		"src/null_init.c",
 		"src/null_joystick.c",
 		"src/null_monitor.c",
 		"src/null_window.c",
+
 		"src/platform.c",
     "src/vulkan.c",
     "src/window.c"
@@ -30,6 +31,7 @@ project "GLFW"
     files {
       "src/win32_init.c",
       "src/win32_joystick.c",
+			"src/win32_module.c",
       "src/win32_monitor.c",
       "src/win32_time.c",
       "src/win32_thread.c",
